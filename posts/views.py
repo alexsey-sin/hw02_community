4a3dc8363c217f5d0b49a836b08978e31aa79baf
alexsey-sin <alexey-sin@yandex.ru>
2021-02-25 19:31:37 +0300

    Fixed_from_25.02.2021_2

--- posts/views.py
+++ posts/views.py
@@ -4,13 +4,13 @@ from .models import Post, Group
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
-    response = render(request, "app/posts/index.html", {"posts": latest})
+    response = render(request, "index.html", {"posts": latest})
     return response
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    response = render(request, "app/posts/group.html",
+    response = render(request, "group.html",
                       {"group": group, "posts": posts})
     return response
