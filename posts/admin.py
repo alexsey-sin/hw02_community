58a18b5d8efc4e7e75da61ce331b856f11a651fa
alexsey-sin <alexey-sin@yandex.ru>
2021-02-26 20:24:36 +0300

    fix01

--- posts/admin.py
+++ posts/admin.py
@@ -3,10 +3,10 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("text", "pub_date", "author")
-    search_fields = ("text",)
-    list_filter = ("pub_date",)
-    empty_value_display = "-пусто-"
+    list_display = ('text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
 
 
 admin.site.register(Post, PostAdmin)
--- posts/views.py
+++ posts/views.py
@@ -3,14 +3,14 @@ from .models import Post, Group
 
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
-    response = render(request, "index.html", {"posts": latest})
+    latest = Post.objects.order_by('-pub_date')[:11]
+    response = render(request, 'index.html', {'posts': latest})
     return response
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    response = render(request, "group.html",
-                      {"group": group, "posts": posts})
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:12]
+    response = render(request, 'group.html',
+                      {'group': group, 'posts': posts})
     return response
